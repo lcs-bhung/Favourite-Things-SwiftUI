3d0df12fb909b8ad40d5c8358d6116c1b22448d2
Branton Hung <BHung@lcs.on.ca>
2021-01-08 14:31:47 -0500

    Initial Commit

---
+++ Favourite Things SwiftUI/Favourite_Things_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Favourite_Things_SwiftUIApp.swift
+//  Favourite Things SwiftUI
+//
+//  Created by Branton Hung on 2021-01-08.
+//
+
+import SwiftUI
+
+@main
+struct Favourite_Things_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
